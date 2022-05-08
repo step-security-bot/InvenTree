4254b87bbed3015bab4938d92a8e486dcac6f818
Matthias <code@mjmair.com>
2022-05-08 13:55:04 +0200

    add named url

--- InvenTree/common/views.py
+++ InvenTree/common/views.py
@@ -6,8 +6,8 @@ from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 from django.utils.translation import gettext_lazy as _
 
+from formtools.wizard.views import SessionWizardView, NamedUrlSessionWizardView
 from crispy_forms.helper import FormHelper
-from formtools.wizard.views import SessionWizardView
 
 from InvenTree.views import AjaxView
 
@@ -75,6 +75,17 @@ class MultiStepFormView(InvenTreeMultiStepMixin, SessionWizardView):
         form_steps_description: description for each form
     """
     pass
+
+
+class NamedMultiStepFormView(InvenTreeMultiStepMixin, NamedUrlSessionWizardView):
+    """ Setup basic methods of multi-step form
+
+        form_list: list of forms
+        form_steps_description: description for each form
+    """
+    pass
+
+
 class FileManagementFormView(MultiStepFormView):
     """File management form wizard.
 
